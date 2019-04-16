76a0cf2abca717894f5921674b2771ac2c7cc66e
nbshelton <nickbshelton@gmail.com>
2019-04-16 15:07:21 -0400

    Fixing potential crash when hashing an object

--- utils.js
+++ utils.js
@@ -1,10 +1,16 @@
-var _ = require( 'lodash' );
 var bson = new ( require( 'bson' ) )( );
 var objhash = require( 'node-object-hash' )( { coerce: false } ).hash;
+var uuid = require( 'uuid/v4' );
 
 exports = module.exports = {};
 
 exports.hash = function ( input ) {
+	try {
+		return objhash( input );
+	} catch ( err ) {
+		// fall back on uuid
+		return uuid( );
+	}
 	return objhash( input );
 };
 
